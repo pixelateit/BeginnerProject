2cb0bae21c117456dbe242205157966de138ce1c
Prakhar Srivastava <prakharsrivastava.vms97@gmail.com>
2020-12-13 20:09:05 +0530

    Second commit

---
+++ Madlib.py
@@ -0,0 +1,6 @@
+# String concatination (aka How to put Strings together)
+# Suppose we want to create a string that says "subscribe______"
+
+youtuber = "PrakahrYT" # Fake youtube channel
+
+print(f"Subscribe to {youtuber}")
